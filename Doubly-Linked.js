6459f488d41368089f173196c559860ff920788f
Taylor Keazirian <taylork26@gmail.com>
2021-04-20 14:13:21 -0500

    added assignments

---
+++ Doubly-Linked.js
@@ -0,0 +1,3 @@
+// Create a queue class using Doubly Linked List
+// Create a queue called starTrekQ and add Kirk, Spock, Uhura, Sulu, and Checkov to the queue.
+// Check to see who is first one on the Queue?
